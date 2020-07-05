1bdeacbe81cb659d5387bd9bd085b5d0a081fc56
Mark <mark@webflow.com>
2020-07-05 13:09:01 +0100

    start adding hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, AnimalDropdown] = useDropdowm;
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form action="">
         <label htmlFor="location">
           Location
@@ -14,6 +19,41 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name="breed"
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
